e67fd427ced529cba4043236398842a16e84ad46
Rozalia Ravnachka <103183688+rozaliya02@users.noreply.github.com>
2022-04-10 01:42:15 +0300

    Add files via upload

---
+++ Basic/Concatena Data.js
@@ -0,0 +1,9 @@
+function concatenaData(input) {
+let FirstName = (input[0]);
+let LastName= (input[1]);
+let age = (input[2]);
+let town = (input[3]);
+ console.log(`You are ${FirstName} ${LastName}, a ${age}-years old person from ${town}.`)
+    
+}
+concatenaData(['Maria', 'Ivanova', '20', 'Sofia'] )
