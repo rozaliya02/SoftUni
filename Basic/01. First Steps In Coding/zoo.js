8fbd8c133ba4afba38503e223beec3f35e2ebb39
Rozalia Ravnachka <103183688+rozaliya02@users.noreply.github.com>
2022-04-11 01:16:52 +0300

    Delete zoo.js

--- Basic/01. First Steps In Coding/zoo.js
+++
@@ -1,7 +0,0 @@
-function zoostore(input) {
-    let dogs = Number(input[0]);
-    let cats = Number(input[1]);
-    let result = dogs * 2.5 + cats * 4;
-    console.log(`${result}`);
-}
-zoostore([5,4]);
