5e53148485a57626a087f49234f6c8cebae287c3
Rozalia Ravnachka <103183688+rozaliya02@users.noreply.github.com>
2022-04-11 01:16:37 +0300

    Delete project creation.js

--- Basic/01. First Steps In Coding/project creation.js
+++
@@ -1,7 +0,0 @@
-function projectCreation(input) {
-    let arcname = input[0];
-    let projects = input[1];
-    let hours = projects * 3;
-    console.log(`The architect ${arcname} will need ${hours} hours to complete ${projects} project/s.`)
-}
-projectCreation(["George", "4"])
